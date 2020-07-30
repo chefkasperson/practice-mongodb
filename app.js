b2db1a90ff244def7e0e47a1ff2071f5aad7c6bf
chefkasperson <chefkasperson@gmail.com>
2020-07-30 14:28:06 -0700

    installed/required mongoose

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-const MongoClient = require('mongodb').MongoClient
-const assert = require('assert')
-
+const mongoose = require('mongoose')
 const url = 'mongodb://localhost:27017'
 
 const dbName = 'fruitsDB'
