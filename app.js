7b2cc616e614653a801717661bd724a46f599b4f
chefkasperson <chefkasperson@gmail.com>
2020-07-31 06:31:16 -0700

    Added validations

--- app.js
+++ app.js
@@ -2,17 +2,25 @@ const mongoose = require('mongoose')
 mongoose.connect("mongodb://localhost:27017/fruitsDB", {useNewUrlParser: true, useUnifiedTopology: true})
 
 const fruitSchema = new mongoose.Schema({
-    name: String,
-    rating: Number,
+    name: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 10
+    },
     review: String
 })
 
 const Fruit = mongoose.model("Fruit", fruitSchema)
-const fruit = new Fruit({
-    name: "Apple",
-    rating: 7,
-    review: "Pretty solid as far as fruits go"
-})
+// const fruit = new Fruit({
+//     name: "Apple",
+//     rating: 7,
+//     review: "Pretty solid as far as fruits go"
+// })
 
 // fruit.save()
 
@@ -22,26 +30,26 @@ const personSchema = new mongoose.Schema({
 })
 
 const Person = mongoose.model("Person", personSchema)
-const person = new Person({
-    name: "John",
-    age: 37
-})
+// const person = new Person({
+//     name: "John",
+//     age: 37
+// })
 
-const kiwi = new Fruit({
-    name: "Kiwi",
-    score: 10,
-    review: "The best fruit!"
-})
-const orange = new Fruit({
-    name: "Orange",
-    score: 8,
-    review: "I like mine a little sour"
-})
-const banana = new Fruit({
-    name: "Banana",
-    score: 7,
-    review: "Comes prepacked"
-})
+// const kiwi = new Fruit({
+//     name: "Kiwi",
+//     score: 10,
+//     review: "The best fruit!"
+// })
+// const orange = new Fruit({
+//     name: "Orange",
+//     score: 8,
+//     review: "I like mine a little sour"
+// })
+// const banana = new Fruit({
+//     name: "Banana",
+//     score: 7,
+//     review: "Comes prepacked"
+// })
 
 // person.save()
 // Fruit.insertMany([kiwi, orange, banana], function(err){
